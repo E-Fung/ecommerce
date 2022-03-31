4fd470fafb3ed809f6c6f828ae5e71480d31f2e4
Eric Fung <ehhfung@gmail.com>
2022-03-31 00:26:35 -0400

    setup the change qty functionality as well as cart page

--- client/src/CartPage.tsx
+++ client/src/CartPage.tsx
@@ -21,10 +21,25 @@ const CartPage: React.FC<Props> = ({ cart }) => {
     setCartProducts(tempDetails);
   };
 
+  const getTotalItems = (): string => {
+    let totalQuantity: number = cartProducts!.reduce(function (total: number, product: Product, index) {
+      return total + cart[index].quantity;
+    }, 0);
+    return `Subtotal (${totalQuantity} item${totalQuantity === 1 ? '' : 's'}):`;
+  };
+
+  const getTotalCost = (): string => {
+    return cartProducts!
+      .reduce(function (total: number, product: Product, index) {
+        return total + product.price * cart[index].quantity;
+      }, 0)
+      .toFixed(2);
+  };
+
   if (!cart.length) {
     return (
       <div className='w-full h-full py-80 flex justify-center'>
-        <div className='text-5xl'>No Items in Cart</div>
+        <div className='text-5xl'>Your cart is empty</div>
       </div>
     );
   }
@@ -32,24 +47,17 @@ const CartPage: React.FC<Props> = ({ cart }) => {
   if (!cartProducts) {
     return <></>;
   }
-  console.log(cartProducts);
+
   return (
     <div className='justify-center flex flex-wrap space-y-2 max-w-7xl mx-auto p-4 sm:px-6 lg:px-8'>
       <div className='bg-white w-full space-y-4'>
-        <div className='text-black'>Shopping Cart</div>
+        <div className='text-black text-xl pl-4 pt-4 font-semibold'>Shopping Cart</div>
         {cartProducts.map((product: Product, index: number) => (
           <CartCard product={product} quantity={cart[index].quantity} key={product.productId} />
         ))}
-        <div className='flex justify-between p-4 border-4 text-black'>
-          <div>Total</div>
-          <div className='font-bold border-2'>
-            Total: $
-            {cartProducts
-              .reduce(function (total: number, product: Product, index) {
-                return total + product.price * cart[index].quantity;
-              }, 0)
-              .toFixed(2)}
-          </div>
+        <div className='flex justify-end p-4 space-x-2 border-4 text-black'>
+          <div className='font-semibold'>{getTotalItems()} </div>
+          <div className='font-bold border-2'>${getTotalCost()}</div>
         </div>
       </div>
     </div>
--- client/src/components/CartCard.tsx
+++ client/src/components/CartCard.tsx
@@ -1,18 +1,103 @@
-import React, { useState, useEffect } from 'react';
-import { Product } from '../models/redux';
+import React, { useState, useEffect, useMemo } from 'react';
+import { Product, CartItem } from '../models/redux';
+import { connect } from 'react-redux';
+import { adjustCart } from '../redux/utils/thunkCreators';
 
-type Props = { product: Product; quantity: number };
+type Props = { product: Product; quantity: number; adjustCart: any };
 
-const CartCard: React.FC<Props> = ({ product, quantity }) => {
+const CartCard: React.FC<Props> = ({ product, quantity, adjustCart }) => {
+  const [tempQty, setTempQty] = useState<number>(quantity);
+  const [select, setSelect] = useState<boolean>(true);
+
+  const handleInputEnter = (event: any) => {
+    const textInput: HTMLElement = document.querySelector('#quantityInput')!;
+    if (event.keyCode === 13) {
+      textInput.blur();
+      updateQuantity(String(tempQty));
+      updateSelect();
+    }
+  };
+
+  const dropdownRange = useMemo(() => {
+    return Array.from({ length: 9 }, (_, i) => i + 1);
+  }, []);
+
+  const updateQuantity = async (quantity: string) => {
+    if (quantity === 'changeType') {
+      setSelect(false);
+    } else {
+      const updatedItem = { quantity: Number(quantity), productId: product.productId };
+      await adjustCart(updatedItem);
+      setTempQty(Number(quantity));
+    }
+  };
+
+  useEffect(() => {
+    updateSelect();
+  }, [quantity]);
+
+  const updateSelect = () => {
+    if (quantity < 10) setSelect(true);
+    else setSelect(false);
+  };
+
+  const deleteItem = () => {};
+
   return (
     <div className='flex text-black w-full p-4 border-4'>
       <div className='h-40 w-40 flex items-center justify-center content-center border-2'>
         <img className='max-h-full max-w-full' src={product?.photoUrl} alt='' />
       </div>
-      <div className='flex w-full justify-between'>
+      <div className='flex w-full ml-4 justify-between'>
         <div className='border-2 flex flex-col'>
-          <div>{product?.name}</div>
-          <div>{quantity}</div>
+          <div className='font-medium'>{product?.name}</div>
+          <div className='flex'>
+            <div className='flex bg-gray-300 rounded text-sm p-1 overflow-clip '>
+              <label htmlFor='quantity'>Qty: </label>
+              {select && (
+                <select
+                  id='quantity'
+                  name='quantity'
+                  defaultValue={quantity}
+                  onChange={(event) => updateQuantity(event.target.value)}
+                  className='text-black bg-gray-300'
+                >
+                  {dropdownRange.map((number) => (
+                    <option value={number} key={number} className='text-black'>
+                      {number}
+                    </option>
+                  ))}
+                  <option value={'changeType'} key={'changeType'} className='text-black'>
+                    +10
+                  </option>
+                </select>
+              )}
+              {!select && (
+                <form
+                  onSubmit={(event) => {
+                    event.preventDefault();
+                    updateQuantity(String(tempQty));
+                    updateSelect();
+                  }}
+                  onKeyDown={(event) => {
+                    handleInputEnter(event);
+                  }}
+                >
+                  <input
+                    id='quantityInput'
+                    name='quantity'
+                    type='number'
+                    value={tempQty}
+                    onChange={(event) => {
+                      setTempQty(Number(event.target.value));
+                    }}
+                  />
+                  <button type='submit'>update</button>
+                </form>
+              )}
+            </div>
+            <div onClick={deleteItem}>Delete</div>
+          </div>
         </div>
         <div className='border-2 font-bold'>${(product?.price! * quantity).toFixed(2)}</div>
       </div>
@@ -20,4 +105,12 @@ const CartCard: React.FC<Props> = ({ product, quantity }) => {
   );
 };
 
-export default CartCard;
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    adjustCart: (params: CartItem) => {
+      dispatch(adjustCart(params));
+    },
+  };
+};
+
+export default connect(null, mapDispatchToProps)(CartCard);
