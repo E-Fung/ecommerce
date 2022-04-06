f2cc856279e7efd19a136a2aa50d6bb9ab7dc84d
Eric Fung <ehhfung@gmail.com>
2022-04-06 14:50:01 -0400

    setup structure of profile page

--- client/src/ProfilePage.tsx
+++ client/src/ProfilePage.tsx
@@ -3,6 +3,7 @@ import { User, State } from './models/redux';
 import { connect } from 'react-redux';
 import { emptyUser } from './assets';
 import { updateUser } from './redux/utils/thunkCreators';
+import { Link } from 'react-router-dom';
 
 type Props = { user: User; updateUser: any };
 
@@ -26,9 +27,31 @@ const ProfilePage: React.FC<Props> = ({ user, updateUser }) => {
   };
 
   return (
-    <div className='justify-center flex flex-wrap max-w-7xl mx-auto p-4 sm:px-6 lg:px-8 text-white'>
-      <img src={user.photoUrl ? user.photoUrl : emptyUser} alt='' />
-      <input type='file' id='image_upload' name='image_upload' accept='image/*' onChange={handleImage} />
+    <div className='flex flex-wrap justify-center max-w-7xl mx-auto p-4 sm:px-6 lg:px-8 text-white'>
+      <div className='flex border-4 p-4'>
+        <div className=''>
+          <div className='flex h-96 w-full justify-center items-center bg-white border-4 rounded-lg p-4'>
+            <img className='max-w-full max-h-full' src={user.photoUrl ? user.photoUrl : emptyUser} alt='' />
+          </div>
+          <div className='flex justify-center'>
+            <div>Change Profile Picture</div>
+          </div>
+          <div className='flex justify-center'>
+            <input type='file' id='image_upload' name='image_upload' accept='image/*' onChange={handleImage} />
+          </div>
+        </div>
+        <div className=''>
+          <div>Name: {user.name}</div>
+          <div>Email: {user.email}</div>
+        </div>
+      </div>
+      <div className='border-4 p-4'>
+        <div>Your Addresses</div>
+        <div>Your Payments</div>
+        <Link to={'/orders'}>
+          <div> Your Orders</div>
+        </Link>
+      </div>
     </div>
   );
 };
