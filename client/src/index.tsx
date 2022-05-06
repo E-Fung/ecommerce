bf0c84002f023bb36677e5f88e17c41a0457824f
Eric Fung <ehhfung@gmail.com>
2022-05-06 18:17:39 -0400

    cloud hosting

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-require('@google-cloud/debug-agent').start({serviceContext: {enableCanary: true}});
 
 ReactDOM.render(
   <React.StrictMode>
